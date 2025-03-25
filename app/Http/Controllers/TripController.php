5ccf48f116e02add2bbd4335b0ebf5e54a1dcf67
gulraizazam <gulraizazam00@gmail.com>
2025-03-25 20:24:42 +0500

    Resoled

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -919,9 +919,9 @@ class TripController extends Controller
 
                 // Reset array keys to ensure a clean array structure
                 $finalFilteredPolyline = array_values($finalFilteredPolyline);
-                    $ftpData = $this->loadAndParseFTPData($finalFilteredPolyline);
-                    $matchingRecords = $this->findMatchingRecords($finalFilteredPolyline, $ftpData);
-                    dd($finalFilteredPolyline);
+                    $matchingRecords = $this->loadAndParseFTPData($finalFilteredPolyline);
+                    //$matchingRecords = $this->findMatchingRecords($finalFilteredPolyline, $ftpData);
+                   
                     $currentTrip = Trip::where('id', $trip->id)->first();
                     $vehicle_id = DriverVehicle::where('driver_id', $currentTrip->user_id)->first();
                     if($vehicle_id){
