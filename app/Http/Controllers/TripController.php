3651d0af607e9df9d5ccc33e16f0d50fe901df9e
gulraizazam <gulraizazam00@gmail.com>
2025-04-10 23:52:34 +0500

    added new branch

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -1163,7 +1163,7 @@ class TripController extends Controller
                 if (!empty($data['routes'][0]['legs'])) {
                     $steps = $data['routes'][0]['legs'][0]['steps'];
                     $decodedCoordinates = [];
-                    $stepSize = 1; // Sample every 10th point
+                    $stepSize = 150; // Sample every 10th point
 
                     foreach ($steps as $step) {
                         if (isset($step['polyline']['points'])) {
