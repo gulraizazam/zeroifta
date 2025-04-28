00918bdc71b2ba5bca783ac9e311e8779ae21bcc
gulraizazam <gulraizazam00@gmail.com>
2025-04-28 22:16:27 +0500

    resolved

--- app/Http/Controllers/CompanyReceiptController.php
+++ app/Http/Controllers/CompanyReceiptController.php
@@ -13,6 +13,7 @@ class CompanyReceiptController extends Controller
     public function index()
     {
         $drivers = User::where('company_id', Auth::id())->pluck('id')->toArray();
+        dd($drivers);
         $trips = Trip::whereIn('user_id', $drivers)->pluck('id')->toArray();
         $receipts = Receipt::with('trip')->whereIn('trip_id', $trips)->get();
         return view('company.receipts.index', compact('receipts'));
